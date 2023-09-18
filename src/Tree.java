98dc12e45d70a5f11ecb89e515849a5eb89bb2d5
Kiarash Kianidehkordi <81038531+kiarashkianid@users.noreply.github.com>
2023-09-18 12:53:33 -0400

    aa

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,29 @@
-public class Tree {
-}
+public class Tree<T> {
+    private T root;
+    private List<Tree<T>> subtrees;
+
+    public Tree(T root) {
+        this.root = root;
+        this.subtrees = new ArrayList<>();
+    }
+
+    public Tree(T root, List<Tree<T>> subtrees) {
+        this.root = root;
+        this.subtrees = new ArrayList<>(subtrees);
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int size() {
+        if (isEmpty()) {
+            return 0;
+        } else {
+            int size = 1;  // count the root
+            for (Tree<T> subtree : subtrees) {
+                size += subtree.size();
+            }
+            return size;
+        }
+    }
